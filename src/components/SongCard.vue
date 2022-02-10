2b9f0f0f6177c8dc416244c041c88f9736f85bf7
MatteoFilippini <matteofilippini8@gmail.com>
2022-02-10 21:47:16 +0100

    created songCard component

---
+++ src/components/SongCard.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="song-card text-center p-3">
+    <figure>
+      <img :src="song.poster" :alt="song.title" class="img-fluid" />
+    </figure>
+    <div class="song-desc">
+      <h4>{{ song.title }}</h4>
+      <p>{{ song.author }}</p>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "SongCard",
+  props: ["song"],
+};
+</script>
+
+<style lang="scss" scoped>
+@import "../assets/scss/-var.scss";
+.song-card {
+  height: 340px;
+  background-color: $header-bg-color;
+  .song-desc {
+    h4 {
+      color: white;
+      text-transform: uppercase;
+    }
+    p {
+      color: darkgray;
+    }
+  }
+}
+</style>
--- src/components/Songs.vue
+++ src/components/Songs.vue
@@ -3,15 +3,7 @@
     <div class="container">
       <div class="row">
         <div class="col-2 p-2" v-for="(song, index) in songs" :key="index">
-          <div class="song-card text-center p-3">
-            <figure>
-              <img :src="song.poster" :alt="song.title" class="img-fluid" />
-            </figure>
-            <div class="song-desc">
-              <h4>{{ song.title }}</h4>
-              <p>{{ song.author }}</p>
-            </div>
-          </div>
+          <SongCard :song="song" />
         </div>
       </div>
     </div>
@@ -19,25 +11,15 @@
 </template>
 
 <script>
+import SongCard from "./SongCard.vue";
 export default {
   name: "Songs",
   props: ["songs"],
+  components: {
+    SongCard,
+  },
 };
 </script>
 
 <style lang="scss" scoped>
-@import "../assets/scss/-var.scss";
-.song-card {
-  height: 340px;
-  background-color: $header-bg-color;
-  .song-desc {
-    h4 {
-      color: white;
-      text-transform: uppercase;
-    }
-    p {
-      color: darkgray;
-    }
-  }
-}
 </style>
